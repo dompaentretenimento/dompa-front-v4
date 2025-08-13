c302fab4f7adadb992f2d02d81442daf714c9f9a
rmdbarbosa <r_mdbarbosa@hotmail.com>
2025-08-13 09:04:17 -0300

    robots blocked env development / envs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,28 @@
 import type { NextConfig } from "next";
 
+import fs from "fs";
+import path from "path";
+
+const env = process.env.NEXT_PUBLIC_ENV;
+const isDevelopment = env === "development";
+
+// Set correct robots.txt before build
+const robotsSource = path.join(
+  process.cwd(),
+  "public",
+  isDevelopment ? "robots.development.txt" : "robots.prod.txt"
+);
+const robotsDest = path.join(process.cwd(), "public", "robots.txt");
+
+if (fs.existsSync(robotsSource)) {
+  fs.copyFileSync(robotsSource, robotsDest);
+  console.log(
+    `✅ robots.txt set for ${
+      isDevelopment ? "DEVELOPMENT (blocked)" : "PRODUCTION (open)"
+    }`
+  );
+}
+
 const nextConfig: NextConfig = {
   // experimental: {
   //   viewTransition: true,

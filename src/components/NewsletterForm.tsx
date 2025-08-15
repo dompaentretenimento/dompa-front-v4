59f90b2a38c47b6f59d1925d0ed0c86a739d6caa
Ricardo Ribeiro <rickyale@gmail.com>
2025-08-15 17:24:21 -0300

    ajuste: ambiente local+dev+prod

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -10,7 +10,7 @@ export default function NewsletterForm() {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API}/front/newsletter/register`,
+        `${process.env.NEXT_PUBLIC_API}/dompa/front/newsletter/register`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },

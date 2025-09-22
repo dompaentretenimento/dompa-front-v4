86eda4071f81dec87de67e158d8f6ebcd2c49a11
Ricardo Ribeiro <rickyale@gmail.com>
2025-09-22 13:04:20 -0300

    ajuste logos + adição de CAA

--- src/components/Home/Parceiros.tsx
+++ src/components/Home/Parceiros.tsx
@@ -2,8 +2,23 @@ import Image from "next/image";
 import Marquee from "react-fast-marquee";
 import React from "react";
 
+const logos = [
+    {src:"logos.svg", title:"Corinhians"},
+    {src:"logos-1.svg", title:"BYD"},
+    {src:"logos-2.svg", title:"Chute Boxe"},
+    {src:"logos-3.svg", title:"Engemon"},
+    {src:"logos-4.svg", title:"Parada SP"},
+    {src:"logos-5.svg", title:"PegPet"},
+    {src:"logos-ccaa.svg", title:"CCAA"},
+    {src:"logos-6.svg", title:"RCN Notícias"},
+    {src:"logos-7.svg", title:"Saúva"},
+    {src:"logos-8.svg", title:"Projeto ORSI"},
+    {src:"logos-9.svg", title:"Santo Mercado"},
+    {src:"logos-10.svg", title:"Studio W"},
+]
+
 export default function Parceiros() {
-  const logos = Array.from({ length: 11 }, (_, i) => i);
+  // const logos = Array.from({ length: 11 }, (_, i) => i);
 
   return (
     <section className="py-xxl desktop:py-xxxl min-h-[232px] desktop:min-h-[224px]">
@@ -12,14 +27,15 @@ export default function Parceiros() {
       </p>
 
       <Marquee speed={125} pauseOnHover={true} autoFill={true}>
-        {logos.map((num) => (
+        {logos.map((logo, num) => (
           <div key={num} className="mr-xl">
+              {/*{logo.src}-{logo.title}*/}
             <Image
-              src={`/home/logos/logos${num === 0 ? "" : `-${num}`}.svg`}
-              alt={`Logo ${num}`}
-              width={100}
-              height={32}
-              style={{ width: "100px", height: "32px" }}
+              src={`/home/logos/${logo.src}`}
+              alt={`Logo ${logo.title}`}
+              width={150}
+              height={48}
+              style={{ width: "150px", height: "48px" }}
               className="w-auto h-auto grayscale"
             />
           </div>
